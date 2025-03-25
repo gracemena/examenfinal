f8fbc55d45ee51c4f364d1752ef2c15e46719800
gracemena <menagrace.mart@gmail.com>
2025-03-24 23:54:44 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,113 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'App Framework7',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/vista/',
+    	url: 'vista.html',
+    	name: 'vista',
+  		},
+		{
+		path: '/acrilicos/',
+    	url: 'acrilicos.html',
+    	name: 'acrilicos',
+  		},
+		
+		{
+		path: '/info/',
+    	url: 'info.html',
+    	name: 'info',
+  		},
+		
+			{
+		path: '/perfil/',
+    	url: 'perfil.html',
+    	name: 'perfil',
+  		}
+		
+		
+		
+	],
+	dialog: {
+		title: 'Diseño Web App',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#modal-login');
+		
+		app.dialog.alert('¡ Bienvenido a la APP !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnCheckout', function (e) {
+	e.preventDefault();
+
+	
+	app.dialog.confirm('¿Seguro desea finalizar su compra?', function () {
+		
+		//app.dialog.alert('Great!');
+		
+		var notification = app.notification.create({
+		  icon: '<i class="material-icons">check</i>',
+		  title: 'Order',
+		  titleRightText: '',
+		  subtitle: '',
+		  text: "Your order has been received.",
+		  closeTimeout: 3000,
+		});
+		notification.open();
+		
+	});
+	
+	
+	
+});
+
+
+
+
+
